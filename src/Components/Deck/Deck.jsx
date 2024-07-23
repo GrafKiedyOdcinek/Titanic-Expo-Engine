40515b25634fc82aff330ca9fe91b9b9e408fe1d
Mathieu <ceran.mete@gmail.com>
2024-07-23 20:29:20 +0200

    chore: Update deck selector buttons position and size in Deck component

--- src/Components/Deck/Deck.jsx
+++ src/Components/Deck/Deck.jsx
@@ -3,14 +3,14 @@ const Deck = ({ selectedDeck, nextDeck, prevDeck }) => {
     <div className="main-deck-container relative p-4">
       <div className="deck-view relative">
         <button
-          className="border rounded-full w-16 h-16 flex justify-center items-center text-center hover:bg-gray-200 hover:text-gray-800 transition-all bg-white absolute left-[5rem] z-20 ripple"
+          className="border rounded-full w-16 h-16 flex justify-center items-center text-center hover:bg-gray-200 hover:text-gray-800 transition-all bg-white absolute left-[-2rem] z-20 ripple"
           onClick={prevDeck}
         >
           <i className="fa-solid fa-arrow-left text-black"></i>
         </button>
 
         <button
-          className="border rounded-full w-16 h-16 flex justify-center items-center text-center hover:bg-gray-200 hover:text-gray-800 transition-all bg-white absolute right-[5rem] z-20 ripple"
+          className="border rounded-full w-16 h-16 flex justify-center items-center text-center hover:bg-gray-200 hover:text-gray-800 transition-all bg-white absolute right-[-2rem] z-20 ripple"
           onClick={nextDeck}
         >
           <i className="fa-solid fa-arrow-right text-black"></i>
